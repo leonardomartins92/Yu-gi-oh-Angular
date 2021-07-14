0856f60270b07cae4a70c8ada5decd505bd57ec5
Leonardo Rodrigues <59699766+leonardomartins92@users.noreply.github.com>
2021-07-14 11:32:01 -0300

    removing surge

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  api: 'https://thirsty-attraction.surge.sh:3000',
+  api: 'http://localhost:3000',
   deckApi: 'http://deckofcardsapi.com/api',
 };
