816896d3152817ad7b3144547dc2ca9120b82151
Leonardo Rodrigues <59699766+leonardomartins92@users.noreply.github.com>
2021-07-14 11:31:44 -0300

    removing surge

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Yu-Gi-Oh
 
-## Surge: https://thirsty-attraction.surge.sh/
+
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 12.1.1.
 
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  api: 'http://localhost:3000',
+  api: 'https://thirsty-attraction.surge.sh:3000',
   deckApi: 'http://deckofcardsapi.com/api',
 };
