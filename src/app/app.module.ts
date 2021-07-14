087da94fa1cc3fa030369adfbf2c96d1692bf096
Leonardo Rodrigues <59699766+leonardomartins92@users.noreply.github.com>
2021-07-14 16:02:09 -0300

    Creating interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { FooterComponent } from './shared/footer/footer.component';
+import { RequestErrorComponent } from './shared/request-error/request-error.component';
+import { RequestErrorInterceptor } from './shared/request-error/request-error.interceptor';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    RequestErrorComponent
   ],
   imports: [
     BrowserModule,
@@ -21,9 +24,14 @@ import { FooterComponent } from './shared/footer/footer.component';
     ReactiveFormsModule,
     AppRoutingModule,
     
-   
   ],
-  providers: [],
+  providers: [
+    {
+    provide: HTTP_INTERCEPTORS,
+    useClass: RequestErrorInterceptor,
+    multi: true,
+    }
+],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/shared/request-error/request-error.component.ts
@@ -0,0 +1,18 @@
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, Input, OnInit } from '@angular/core';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'app-request-error',
+  templateUrl: './request-error.component.html',
+  styleUrls: ['./request-error.component.scss']
+})
+export class RequestErrorComponent implements OnInit {
+
+  @Input() error!: HttpErrorResponse;
+  constructor(public activeModal: NgbActiveModal) {
+  }
+  ngOnInit(): void {
+  }
+
+}
